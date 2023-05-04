5a70804e34073136cb96f706b0f55bfa7342e761
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2023-05-04 22:44:22 +0530

    go semicolon batch
    go
    semicolon
    batch

---
+++ 52_go_semicolon_statement_and_batch.sql
@@ -0,0 +1,14 @@
+create table qwerty(a int,b int,c int)
+
+
+
+
+insert into qwerty(a,b,c)
+values(1,2,3)
+go 5
+
+select * from qwerty
+
+create procedure p3 as select 'first stored procedure1'
+GO 
+create procedure p4 as select 'second stored procedure2'
