7d267ab9fa88634f2ba2397f5e1790132275c456
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2023-05-15 22:30:38 +0530

    string functions part 1
    string functions part 1

---
+++ 55_string_functions_part_1.sql
@@ -0,0 +1,30 @@
+CONCAT --returns a string resulting from the concatenation, or joining, of two or more string values
+--CONCAT ( string_value1, string_value2 [, string_valueN ] )  
+
+CONCAT_WS --same as concat, but we can give seperator as an argument
+--CONCAT_WS ( separator, argument1, argument2 [, argumentN]... )
+
+LEFT--Returns the left part of a character string with the specified number of characters.
+--LEFT ( character_expression , integer_expression )  
+
+RIGHT--Returns the right part of a character string with the specified number of characters.
+--RIGHT ( character_expression , integer_expression )
+
+PATINDEX--Returns the starting position of the first occurrence of a pattern in a specified expression
+--PATINDEX ( '%pattern%' , expression )  
+
+
+REPLICATE--Repeats a string value a specified number of times.
+--REPLICATE ( string_expression , integer_expression ) 
+
+REVERSE--Returns the reverse order of a string value.
+--REVERSE ( string_expression )  
+
+SUBSTRING--Returns part of a character, binary, text
+--SUBSTRING ( expression ,start , length )  
+
+LOWER--Returns a character expression after converting uppercase character data to lowercase.
+--LOWER ( character_expression )  
+
+UPPER--Returns a character expression with lowercase character data converted to uppercase.
+--UPPER ( character_expression )  
