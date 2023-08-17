84b52a128c8d6f8eb6ea1d21c3df9b98a5520395
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2023-08-17 23:06:50 +0530

    insert_update_target_table_with_merge
    insert_update_target_table_with_merge

---
+++ 71_insert_update_target_table_with_merge.sql
@@ -0,0 +1,28 @@
+--update target table based on source table
+--using merge
+SELECT * FROM SourceProducts
+SELECT * FROM TargetProducts
+
+use MergeDemo
+go
+
+merge into targetproducts as target
+using sourceproducts as source
+on target.productid = source.productid
+--for insert
+when not matched then
+	insert(productid,productname,price)
+	values(source.productid,source.productname,source.price)
+--for update
+when matched then
+	update set 
+		target.productname = source.productname,
+		target.price = source.price;
+
+select * from targetproducts
+select * from sourceproducts
+
+--https://www.sqlshack.com/understanding-the-sql-merge-statement/
+
+
+
