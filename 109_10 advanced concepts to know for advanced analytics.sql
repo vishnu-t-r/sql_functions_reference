b8b0e77ae699894e0deecd808fa5934e9d7926f5
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-01-20 21:00:15 +0530

    concepts to know for advanced sql analytics
    advanced concepts to know for advanced sql analytics

---
+++ 109_10 advanced concepts to know for advanced analytics.sql
@@ -0,0 +1,13 @@
+-- 10 advanced concepts we should know for data science and analytics
+
+--cte
+--window functions
+--joins with exclusions
+--running totals
+--date functions and manipulations
+--pivot data
+--unpivot data
+--correlated subquery
+--dealing with duplicate data
+--temporary tables
+
