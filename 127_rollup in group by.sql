0fa48f0362b9867b7421d472116645374b8c3c8d
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-02-13 11:05:45 +0530

    rollup in group by

---
+++ 127_rollup in group by.sql
@@ -0,0 +1,38 @@
+--ROLLUP IN SQL
+
+/*
+SELECT column1, column2, ..., AGGREGATE_FUNCTION(column)
+FROM table
+GROUP BY ROLLUP(column1, column2, ...);
+*/
+
+--select * from cube_sales;
+
+select product, region, sum(salesamount) as total_sales
+from cube_sales
+group by rollup(product, region);
+
+--aggregate by product and region
+--calculate sub-total by product
+--grand total for all rows
+
+select product, region, sum(salesamount) as total_sales
+from cube_sales
+group by rollup(region, product);
+
+--aggregate by product and region
+--calculate sub-total by region
+--grand total for all rows
+
+/*
+Key Differences Between ROLLUP and CUBE:
+
+ROLLUP:
+Produces subtotals in a hierarchical manner (left-to-right in the GROUP BY list).
+Generates fewer rows compared to CUBE.
+
+CUBE:
+Produces all possible combinations of groupings.
+Includes subtotals for all possible subsets of columns.
+
+*/
