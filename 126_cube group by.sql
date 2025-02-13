9915fbc04df8d0192e6fbaeddd1cfe66721e6228
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-02-13 11:05:22 +0530

    cube group by

---
+++ 126_cube group by.sql
@@ -0,0 +1,33 @@
+--CUBE IN SQL
+--Generate all possible combinations of groupings for the result set
+use int_ques;
+/*
+SELECT column1, column2, ..., AGGREGATE_FUNCTION(column)
+FROM table
+GROUP BY CUBE(column1, column2, ...);
+*/
+
+/*
+use int_ques;
+
+create table cube_sales(
+product varchar(20),
+region varchar(25),
+salesamount int
+);
+
+insert into cube_sales(product, region, salesamount)
+values('A',	'North',	100),
+('A',	'South',	150),
+('B',	'North',	200),
+('B',	'South',	250)
+*/
+
+--We want to get the total sales for each product, region, and combinations of these?
+
+--select * from cube_sales;
+
+
+select product, region, sum(salesamount) as total_sales
+from cube_sales
+group by cube(product, region);
